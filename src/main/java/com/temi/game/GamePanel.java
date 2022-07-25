f3194f4f17551e9fbc7e5501bc86a9fa9caafdb4
temitopejegede <temiteejegede@gmail.com>
2022-07-25 13:33:22 -0400

    added method signatures and inner class

--- src/main/java/com/temi/game/GamePanel.java
+++ src/main/java/com/temi/game/GamePanel.java
@@ -1,4 +1,50 @@
 package com.temi.game;
 
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 public class GamePanel {
+
+    GamePanel(){
+
+    }
+
+    public void newBall(){
+
+    }
+
+    public void newPaddles(){
+
+    }
+
+    public void paint(Graphics g){
+
+    }
+
+    public void draw(Graphics g){
+
+    }
+
+    public void move(){
+
+    }
+
+    public void checkCollision(){
+
+    }
+
+    public void run(){
+
+    }
+
+    public class AL extends KeyAdapter{
+        public void keyPressed(KeyEvent e){
+
+        }
+
+        public void keyReleased(KeyEvent e){
+
+        }
+    }
 }
