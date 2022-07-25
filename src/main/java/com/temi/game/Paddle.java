7df707b6221959e86e05ef0f500926c9f6c24aa1
temitopejegede <temiteejegede@gmail.com>
2022-07-25 14:26:23 -0400

    added constructor and methods

--- src/main/java/com/temi/game/Paddle.java
+++ src/main/java/com/temi/game/Paddle.java
@@ -1,4 +1,30 @@
 package com.temi.game;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 public class Paddle {
+    Paddle(){
+
+    }
+
+    public void keyPressed(KeyEvent e){
+
+    }
+
+    public void keyReleased(KeyEvent e){
+
+    }
+
+    public void setYDirection(int yDirection){
+
+    }
+
+    public void move(){
+
+    }
+
+    public void draw(Graphics g){
+
+    }
 }
