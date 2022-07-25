8ffcec0ab4a0786f8c919f0ebdc819adcca48400
temitopejegede <temiteejegede@gmail.com>
2022-07-25 14:26:11 -0400

    added constructor and methods

--- src/main/java/com/temi/game/Ball.java
+++ src/main/java/com/temi/game/Ball.java
@@ -1,4 +1,27 @@
 package com.temi.game;
 
+import java.awt.*;
+
 public class Ball {
+
+    Ball(){
+
+    }
+
+    public void setXDirection(int randomXDirection){
+
+    }
+
+    public void setYDirection(int randomYDirection){
+
+    }
+
+    public void move(){
+
+    }
+
+    public void draw(Graphics g){
+
+    }
 }
+
