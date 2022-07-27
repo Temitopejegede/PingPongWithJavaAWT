635bd4a2feeb9a910bee5bb9402f3fbf50dda495
temitopejegede <temiteejegede@gmail.com>
2022-07-26 21:52:22 -0400

    these define how fast will be mvoing on the x and y axis

--- src/main/java/com/temi/game/Ball.java
+++ src/main/java/com/temi/game/Ball.java
@@ -1,8 +1,13 @@
 package com.temi.game;
 
 import java.awt.*;
+import java.util.Random;
 
-public class Ball {
+public class Ball extends Rectangle{
+
+    Random random;
+    int xVelocity;
+    int yVelocity;
 
     Ball(){
 
