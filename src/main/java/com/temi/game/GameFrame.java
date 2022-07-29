2e518ed531da191104e2514ea4ab07bb0ac693ab
temitopejegede <temiteejegede@gmail.com>
2022-07-28 22:43:28 -0400

    added the game frame window

--- src/main/java/com/temi/game/GameFrame.java
+++ src/main/java/com/temi/game/GameFrame.java
@@ -1,12 +1,21 @@
 package com.temi.game;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameFrame extends JFrame {
 
-    GamePanel panel = new GamePanel();
+    GamePanel panel;
 
     GameFrame(){
-
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Ping Pong Game");
+        this.setResizable(false);
+        this.setBackground(Color.BLACK);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
     }
 }
